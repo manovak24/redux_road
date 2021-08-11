76d34487e7a4f3cb9c9f2a0051a921e315ecc6b7
emagdnimMQT <mark.w.novak@gmail.com>
2021-08-11 10:05:13 -0400

    Almost complete. Still a few bugs.

--- redux-road.js
+++ redux-road.js
@@ -14,8 +14,55 @@ const gameReducer = (state = initialWagonState, action) => {
                 days: state.days + 1
             }
         }
+
+        case 'travel': {
+            if (state.supplies > 0) {
+                return {
+                    ...state,
+                    supplies: state.supplies - (20 * action.payload),
+                    distance: state.distance + (10 * action.payload),
+                    days: action.payload
+                }
+            } else if (state.supplies <= -1) {
+                return [...state]
+            }  
+        }
+
+        case 'tippedWagon': {
+            return {
+                ...state,
+                supplies: state.supplies - 30,
+                distance: state.distance,
+                days: state.days + 1
+            }
+        }
+
         default: {
             return state;
         }
     }
 }
+
+let wagon = gameReducer(undefined, {});
+
+wagon = gameReducer(wagon, {
+    type: 'travel',
+    payload: 1
+})
+
+wagon = gameReducer(wagon, {
+    type: 'gather'
+})
+
+wagon = gameReducer(wagon, {
+    type: 'tippedWagon'
+})
+
+wagon = gameReducer(wagon, {
+    type: 'travel',
+    payload: 3
+})
+
+
+console.log(wagon);
+
